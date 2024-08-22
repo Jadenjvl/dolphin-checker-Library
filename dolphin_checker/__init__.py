875005994a3a41c1a30e85b0474b5eef9eaf655d
Jaden <140992942+Jaden-og@users.noreply.github.com>
2024-08-22 18:17:03 -0400

    Delete dolphin_checker directory

--- dolphin_checker/__init__.py
+++
@@ -1,5 +0,0 @@
-from .os_check import check_os
-from .cpu_check import check_cpu
-from .ram_check import check_ram
-from .gpu_check import check_gpu
-from .checker import run_check
--- dolphin_checker/cpu_check.py
+++
@@ -1,8 +0,0 @@
-import cpuinfo
-
-def check_cpu():
-    cpu_info = cpuinfo.get_cpu_info()
-    if cpu_info['bits'] == 64 and cpu_info['hz_actual_friendly']:
-        return f"CPU: {cpu_info['brand_raw']} ({cpu_info['hz_actual_friendly']}) - Supported"
-    else:
-        return f"CPU: {cpu_info['brand_raw']} ({cpu_info['hz_actual_friendly']}) - Not Supported"
